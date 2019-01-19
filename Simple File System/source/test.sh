47cce9b35f433837f92c098420db3aefcae6c8fe
Minkyun Kim <mkk0618@naver.com>
2019-01-19 14:12:18 +0900

    Rename dir

--- Simple File System/source/test.sh
+++
@@ -1,9 +0,0 @@
-cc -o mysfs sfs_disk.c sfs_func_hw.c sfs_main.c sfs_func_ext.o 
-cp ../img/* .
-mysfs < $1 > my.txt
-rm ok12sfs
-cp ../img/* .
-sfs < $1 > ans.txt
-rm ok12sfs
-diff my.txt ans.txt
-cp ../img/* .
